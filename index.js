3f83b51f1a6bea2a8c18fd0d24447d8188e069e1
DoktorD96 <dusanruzicc@gmail.com>
2021-04-19 15:58:03 +0200

    testing new code performacn

--- index.js
+++ index.js
@@ -1,86 +1,139 @@
-//const browserObject = require('./browser');
-//const scraperController = require('./pageController')
-//Start the browser and create a browser instance
-//let browserInstance = browserObject.startBrowser();
-// Pass the browser instance to the scraper controller
-//scraperController(browserInstance)
-//const fs = require('fs');
-const Xvfb = require('xvfb');
+var config = require('./modules/config');
+
+if (config.mode == "server") {
+    const Xvfb = require('xvfb');
+}
+if (config.mode == "local") {
+
+}
+if (config.mode == "test") {
+    config.log = true;
+}
+
+config.log = true;
+
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
-puppeteer.use(StealthPlugin())
-var browser = null;
-function sleep(sleep){
-    return new Promise(function(resolve) {
-        setTimeout(function(){resolve("true");}, sleep*1000);
+puppeteer.use(StealthPlugin());
+
+var browsercon = null;
+function sleep(sleep) {
+    return new Promise(function (resolve) {
+        setTimeout(function () { resolve("true"); }, sleep * 1000);
     });
 }
 var scrapped = [];
-var sc_len = 0;
-var nodeLog = function(logg) { console.log(logg); };
+var sc_len = -1;
+
+var nodeLog = function (logg) {
+    console.log(logg);
+};
+
 (async () => {
     try {
-    var xvfb = new Xvfb({
-        silent: true,
-        xvfb_args: ["-screen", "0", '800x600x24', "-ac"],
-    });
-    xvfb.start((err)=>{if (err) console.error(err)})
-	browser = await puppeteer.launch({
-        headless: false,
-        defaultViewport: null,
-        args: ['--disable-setuid-sandbox','--no-sandbox', '--start-fullscreen','--display='+xvfb._display],
-        ignoreHTTPSErrors: true
-        });
-        console.log("Opening the browser......");
-    }catch (err){
-        console.log("Could not create a browser instance => : ", err);
-    }
-    const page = await browser.newPage();
-    await page.exposeFunction("nodeLog", nodeLog);
-    await page.goto(`https://www.tiktok.com/tag/makeup?is_copy_url=1&is_from_webapp=v1`,{});
-    //await page.evaluate(async({}) => {
-       //         try{
-		//await nodeLog(document.querySelector("div.share-layout-content").innerHTML.trim());
-		//setInterval(async function(){
-		//	try{
-                 //       await nodeLog("-------------------");
-                   ///     document.documentElement.scrollTop = 1e35;
-			//var a = document.querySelectorAll("div.video-feed-item a[href]").length;
-			//await nodeLog(a);
-                        //var b = document.querySelectorAll("a.video-feed-item-wrapper[href]").length;
-                        //await nodeLog(b);
-		//	scrapped = [];
-//if(document.querySelectorAll("div.video-feed-item a[href]").length == sc_len){
-//			document.querySelectorAll("div.video-feed-item a[href]").forEach(async function (element){
-//	let t = element.getAttribute("href");
-//	t = t.substring(t.indexOf("@"));
-//	t = t.substring(0,t.indexOf("/"));
-//	scrapped.push(t);
-//		}
-//		);
-//await nodeLog("Finall");
-//await nodeLog(scrapped.length);
-//}else{
-//sc_len = document.querySelectorAll("div.video-feed-item a[href]").length;
-//await nodeLog(sc_len);
-//}
+        if (config.mode == "server") {
+            var xvfb = new Xvfb({
+                silent: true,
+                xvfb_args: ["-screen", "0", '800x600x24', "-ac"],
+            });
+            xvfb.start((err) => { if (err && config.log) console.error(err) })
+        }
 
-  //                      }catch(e){await nodeLog("error 1\n");await nodeLog(JSON.stringify(e));}
-//		},10000);
-  //              }catch(e){await nodeLog("error 2\n");await nodeLog(JSON.stringify(e));}
-    //},{});
-    //await page.setDefaultTimeout(5*60*60);
-    //const base64 = await page.screenshot({ encoding: "base64" });
-const image =  await page.screenshot({
-quality: 65,
-type: "jpeg",
-encoding: "base64"
-});
-//console.log(image);
-//fs.writeFile('img.txt', image, function (err) {
-//  if (err) return console.log(err);
-//});
-   // await sleep(5*60*60);
-    await browser.close()
-    xvfb.stop();
+        var configpup = config.settings;
+        if (config.mode == "server") {
+            configpup.args.push('--display=' + xvfb._display);
+            configpup.args.push('--single-process');
+            configpup.args.push('--start-fullscreen');
+        }
+
+        if (config.mode == "local") {
+            configpup.args.push("--window-size=200,200");
+        }
+
+
+
+        browsercon = await puppeteer.launch(configpup);
+        //browsercon = await browser.createIncognitoBrowserContext();
+
+        if (config.log) {
+            console.log("Opening the browser......");
+        }
+    } catch (err) {
+        if (config.log) {
+            console.log("Could not create a browser instance => : ", err);
+        }
+    }
+    const pages = await browsercon.pages();
+    const page = pages[0];
+
+
+
+
+    await page.exposeFunction("nodeLog", nodeLog);
+    await page.goto(`https://www.tiktok.com/tag/makeup?is_copy_url=1&is_from_webapp=v1`, {});
+    await page.evaluate(async ({ scrapped, sc_len }) => {
+        try {
+            //await nodeLog(document.querySelector("div.share-layout-content").innerHTML.trim());
+            setInterval(async function () {
+                await nodeLog("interval");
+                try {
+                    document.documentElement.scrollTop = 1e35;
+                    var a = document.querySelectorAll("div.video-feed-item a[href]").length;
+                    //document.querySelectorAll("a.video-feed-item-wrapper[href]").length;
+                    if (true || config.log) {
+                        //await nodeLog(a);
+                    }
+                    scrapped = [];
+                    if (document.querySelectorAll("div.video-feed-item a[href]").length == sc_len) {
+                        await nodeLog("equal");
+                        document.querySelectorAll("div.video-feed-item a[href]").forEach(async function (element) {
+                            let t = element.getAttribute("href");
+                            t = t.substring(t.indexOf("@"));
+                            t = t.substring(0, t.indexOf("/"));
+                            scrapped.push(t);
+                        }
+                        );
+                        if (true || config.log) {
+                            await nodeLog("Finall");
+                            await nodeLog(scrapped.length);
+                        }
+                    } else {
+                        await nodeLog("not equal");
+                        if (true || config.log) {
+                            sc_len = document.querySelectorAll("div.video-feed-item a[href]").length;
+                            await nodeLog(sc_len);
+                        }
+                    }
+
+                } catch (e) {
+                    await nodeLog(e.message);
+                    if (config.log) {
+                        await nodeLog("error 1");
+                        await nodeLog(JSON.stringify(e));
+                    }
+                }
+            }, 10000);
+        } catch (e) {
+            if (config.log) {
+                await nodeLog("error 2");
+                await nodeLog(JSON.stringify(e));
+            }
+        }
+    }, { scrapped, sc_len });
+    await page.setDefaultTimeout(5 * 60 * 60);
+    const image = await page.screenshot({
+        quality: 65,
+        type: "jpeg",
+        encoding: "base64"
+    });
+    if (config.log && false) {
+        console.log(image);
+    }
+    await sleep(5 * 60 * 60);
+    await browsercon.close();
+
+    if (config.mode == "server") {
+        xvfb.stop();
+    }
+
 })()
---
+++ modules/config.js
@@ -0,0 +1,54 @@
+const MODE = process.env.MODE;
+
+
+const config = {
+    log: false,
+    mode: MODE,
+    settings: {
+
+        ignoreDefaultArgs: ["--disable-extensions"],
+
+        headless: false,
+        defaultViewport: null,
+        ignoreHTTPSErrors: true,
+        args: [
+            //--user-data-dir=./proxy_data_save,
+            //"--window-size=200,200",
+            //*'--single-process',*/
+            '--disable-web-security',
+            '--ignore-certificate-errors',
+            '--disable-infobars',
+            '--disable-notifications',
+            '--allow-insecure-localhost',
+            '--disable-device-discovery-notifications',
+            '--allow-file-access-from-files',
+            '--media-cache-size=0',
+            '--disk-cache-size=0',
+            '--aggressive-cache-discard',
+            '--disable-cache',
+            '--disable-application-cache',
+            '--disable-offline-load-stale-cache',
+            '--disable-gpu-shader-disk-cache',
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--disable-gpu',
+            '--mute-audio',
+            '--proxy-server="direct://"',
+            '--proxy-bypass-list=*',
+            '--js-flags="--max-old-space-size=1024"',
+            "--disable-web-security",
+            "-–allow-file-access-from-files",
+            "--disable-site-isolation-trials",
+            "--allow-file-access",
+            "--allow-cross-origin-auth-prompt",
+            "--incognito"
+        ]
+    },
+
+}
+
+module.exports = config;
