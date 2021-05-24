2379ef0f5082e30ffb85483c8e9c60666a5575c8
DoktorD96 <dusanruzicc@gmail.com>
2021-05-24 04:42:04 +0200

    update

--- modules/start_app.js
+++ modules/start_app.js
@@ -6,7 +6,7 @@ module.exports.startfn = async function () {
 
 
     await TIK.start();
-    await TIK.openpage("https://www.tiktok.com/tag/makeup?is_copy_url=1&is_from_webapp=v1", false);
+    await TIK.openpage("https://www.tiktok.com/tag/makeup?is_copy_url=1&is_from_webapp=v1", true);
     await TIK.infinitescrool();
     await TIK.sleep(60 * 60); // 60 min
     await TIK.close();
